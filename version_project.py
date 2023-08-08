9b42d14874c98fe602edd805a2581cd1a08bb5bb
aksoybora <bora.aaksoy@gmail.com>
2023-08-08 16:54:00 +0300

    changed language & added workspace path

--- version_project.py
+++ version_project.py
@@ -4,138 +4,147 @@ import re
 import json
 import platform
 
+workspace_path = ".."
 
 def main():
     try:
         if len (sys.argv) < 2:
-            print("\nHATA: Lutfen bir komut belirtin!!\n")
+            print("\nERROR: Please enter a command!\n")
             return
         
-        komut = sys.argv[1]
+        command = sys.argv[1]
 
-        if komut == "save":
+        if command == "save":   
             if len(sys.argv) < 3:
-                print("\nHATA: Lutfen klasor ismini belirtin!\n")
+                print("\nERROR: Please specify a folder name!\n")
                 return
             
-            klasor_ismi = sys.argv[2]
-            #txt_oku_ve_kaydet(klasor_ismi)
-            versiyon_bilgisi_degeri = versiyon_bilgisini_al(klasor_ismi)
-            json_kaydet(klasor_ismi, versiyon_bilgisi_degeri, txt_oku_ve_kaydet(klasor_ismi))
+            folder_name = sys.argv[2]
+            folder_name = os.path.join(workspace_path, folder_name)
+            version_info_value = get_version_info(folder_name)
+            save_to_json(folder_name, version_info_value, txt_read(folder_name))
         
         else:
-            print("\nHATA: Gecersiz Komut!\n")
+            print("\nERROR: Invalid command!\n")
     except Exception as e:
-        print("Bir hata olustu:",e)
+        print("Something went wrong:",e)
 
 
-def txt_oku_ve_kaydet(klasor_ismi):
+def txt_read(folder_name):
     try:
-        txt_dosyasi = "CMakeLists.txt"
-        txt_yolu = os.path.join(klasor_ismi, txt_dosyasi)
+        txt_file = "CMakeLists.txt"
+        txt_path = os.path.join(folder_name, txt_file)
 
-        if os.path.exists(txt_yolu):
-            with open(txt_yolu, "r") as dosya:
-                alt_satir = False
-                alinan_degerler = []
-                for line in dosya:
+        print("AAA" + txt_path)
+
+        if os.path.exists(txt_path):
+            with open(txt_path, "r") as file:
+                bottom_line = False
+                received_values = []
+                for line in file:
                     curline = line.strip()
-                    if alt_satir:
+                    if bottom_line:
                         if "{CMAKE_CURRENT_SOURCE_DIR}" in curline:
                             # Eşleşme bulundu, burada yapmak istediğiniz işlemi yapabilirsiniz
-                            dosya_yolu = curline.replace("${CMAKE_CURRENT_SOURCE_DIR}/../", "").strip()
-                            alinan_degerler.append(dosya_yolu)
+                            file_path = curline.replace("${CMAKE_CURRENT_SOURCE_DIR}/../", "").strip()
+                            received_values.append(file_path)
                         else:
                             # Başka bir ifade geldiğinde alt satırdan çık
-                            alt_satir = False
+                            bottom_line = False
                     elif curline.startswith("include_directories(${PROJECT_NAME} PUBLIC"):
                         # "include_directories(${PROJECT_NAME} PUBLIC" ile başlayan satır bulundu
-                        alt_satir = True
+                        bottom_line = True
             a = {}
-            for dosya_yolu in alinan_degerler:
+            for file_path in received_values:
                 try:
-                    versiyonlar = versiyonları_al(alinan_degerleri_oku(dosya_yolu))
-                    if versiyonlar:
-                        dosya_yolu = dosya_yolu.split("/")[0]
-                        a[dosya_yolu] = versiyonlar
+                    versions = get_versions(read_received_values(file_path))
+                    if versions:
+                        file_path = file_path.split("/")[0]
+                        a[file_path] = versions
                 except Exception as e:
-                    print("Hata:", e)
+                    print("ERROR:", e)
                     pass
             return a
     except Exception as e:
-        print("HATA! txt_oku_ve_kaydet:",e)
+        print("ERROR! txt_read:",e)
         return None
     
 
-def alinan_degerleri_oku(dosya_yolu):
+def read_received_values(file_path):
     try:
         if platform.system() == "Windows":
-            dosya_yolu = dosya_yolu.replace("/", "\\")  # Ters slash'ları normal slash'a dönüştür
-        version_h_yolu = os.path.join(dosya_yolu,'version.h')
-        if os.path.exists(version_h_yolu):
-            with open(version_h_yolu,"r") as dosya3:
-                version_h_larin_icerikleri = dosya3.read()
-                return version_h_larin_icerikleri
+            file_path = file_path.replace("/", "\\")  # Ters slash'ları normal slash'a dönüştür
+            print("ilk ife girdim :P\n")
+
+        version_h_path = os.path.join(workspace_path,file_path)
+        version_h_path = os.path.join(version_h_path,'version.h')
+        print("BURADAYIM! "+version_h_path)
+
+        if os.path.exists(version_h_path):
+            with open(version_h_path,"r") as file3:
+                contents_of_version_h = file3.read()
+                print("ikinci ife girdim "+ contents_of_version_h)
+                return contents_of_version_h
         else:
-            print("version.h bulunamadi " + dosya_yolu)
+            print("version.h not found: " + file_path)
     except Exception as e:
-        print("HATA! alinan_degerleri_oku:",e)
+        print("ERROR! read_received_values:",e)
         return None
 
 
-def versiyonları_al(version_h_larin_icerikleri):
+def get_versions(contents_of_version_h):
     try:
-        if version_h_larin_icerikleri:
-            majorv = re.search(r'#define\s+MAJOR_VERSION\s+(\d+)', version_h_larin_icerikleri)
-            minorv = re.search(r'#define\s+MINOR_VERSION\s+(\d+)', version_h_larin_icerikleri)
-            patchv = re.search(r'#define\s+PATCH_VERSION\s+(\d+)', version_h_larin_icerikleri)
+        if contents_of_version_h:
+            majorv = re.search(r'#define\s+MAJOR_VERSION\s+(\d+)', contents_of_version_h)
+            minorv = re.search(r'#define\s+MINOR_VERSION\s+(\d+)', contents_of_version_h)
+            patchv = re.search(r'#define\s+PATCH_VERSION\s+(\d+)', contents_of_version_h)
 
             if majorv and minorv and patchv:
                 # Değeleri alıp istenen formatta versiyon bilgisini oluşturma
-                versiyon_bilgisi_d = f"{majorv.group(1)}.{minorv.group(1)}.{patchv.group(1)}"
-                return versiyon_bilgisi_d
+                version_info_d = f"{majorv.group(1)}.{minorv.group(1)}.{patchv.group(1)}"
+                return version_info_d
             else:
-                print("version bilgisini bulamadim")
+                print("Not found version info!")
     except Exception as e:
-        print("HATA! versiyonları_al:",e)
+        print("ERROR! get_versions:",e)
         return None
         
 
-def versiyon_bilgisini_al(klasor_ismi):
+def get_version_info(folder_name):
     try:
-        include_yolu = os.path.join(klasor_ismi, "include")
+        include_path = os.path.join(folder_name, "include")
 
-        if os.path.exists(include_yolu):
-            version_yolu = os.path.join(include_yolu, "version.h")
+        if os.path.exists(include_path):
+            version_path = os.path.join(include_path, "version.h")
 
-            if os.path.exists(version_yolu):
-                with open(version_yolu,"r") as dosya_2:
-                    icerik = dosya_2.read()
+            if os.path.exists(version_path):
+                with open(version_path,"r") as file2:
+                    content = file2.read()
                     # Düzenli ifadeyle MAJOR_VERSION, MINOR_VERSION ve PATCH_VERSION değerlerini bulma
-                    major = re.search(r'#define\s+MAJOR_VERSION\s+(\d+)', icerik)
-                    minor = re.search(r'#define\s+MINOR_VERSION\s+(\d+)', icerik)
-                    patch = re.search(r'#define\s+PATCH_VERSION\s+(\d+)', icerik)
+                    major = re.search(r'#define\s+MAJOR_VERSION\s+(\d+)', content)
+                    minor = re.search(r'#define\s+MINOR_VERSION\s+(\d+)', content)
+                    patch = re.search(r'#define\s+PATCH_VERSION\s+(\d+)', content)
 
                     if major and minor and patch:
                         # Değeleri alıp istenen formatta versiyon bilgisini oluşturma
-                        versiyon_bilgisi = f"{major.group(1)}.{minor.group(1)}.{patch.group(1)}"
+                        version_info = f"{major.group(1)}.{minor.group(1)}.{patch.group(1)}"
 
                     else:
-                        print("\nHATA: Versiyon bilgileri bulunamadı!\n")
+                        print("\nERROR: Not found version informations!\n")
 
             else:
-                print("\nHATA: 'version.h' dosyası bulunamadı!\n")
+                print("\nERROR: Not found 'version.h'!\n")
 
         else:
-            print(f"\nHATA: {klasor_ismi} icinde 'include' adinda bir klasor bulunamadi!\n")
+            print(f"\nERROR: No folder named 'include' found in {folder_name}!\n")
 
-        return versiyon_bilgisi
+        return version_info
     except Exception as e:
-        print("HATA! versiyon_bilgisini_al:",e)
+        print("ERROR! get_version_info:",e)
         return None
 
 
-def json_kaydet(klasor_ismi, versiyon_bilgisi, a={}):
+def save_to_json(folder_name, version_info, a={}):
     try:
         try:
             with open("output.json", "r") as json_file:
@@ -143,18 +152,18 @@ def json_kaydet(klasor_ismi, versiyon_bilgisi, a={}):
         except FileNotFoundError:
             data = {}
 
-        if klasor_ismi not in data:
-            data[klasor_ismi] = {}
+        if folder_name not in data:
+            data[folder_name] = {}
 
-        if "version " + versiyon_bilgisi not in data[klasor_ismi]:
-            data[klasor_ismi]["version " + versiyon_bilgisi] = {}
+        if "version " + version_info not in data[folder_name]:
+            data[folder_name]["version " + version_info] = {}
 
-        data[klasor_ismi]["version " + versiyon_bilgisi].update(a)
+        data[folder_name]["version " + version_info].update(a)
 
         with open("output.json", "w") as json_file:
             json.dump(data, json_file, indent=3)
     except Exception as e:
-        print("HATA! json_kaydet:",e)
+        print("ERROR! save_to_json:",e)
 
 
 if __name__ == "__main__":
